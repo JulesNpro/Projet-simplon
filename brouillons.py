0a97dffbbd6b65034472736b9267ead3471da224
JulesNpro <jules.ndiaye.pro@gmail.com>
2024-09-20 11:02:13 +0200

    password chek

--- brouillons.py
+++ brouillons.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from werkzeug.security import generate_password_hash 
 from werkzeug.security import check_password_hash
+import getpass
 
 hashed_password = 'scrypt:32768:8:1$F3XsEFqOikiQr56T$614e64d91b7bcc6802eafdba3e7d00a0187f9c45273621bb838da53943245c5a071d4624495895eede3877c2a4e84955314ba0efaede927a8ec7c7a0a73917b1'
 
@@ -11,8 +12,6 @@ admin = {
 }
 
 admin_df = pd.DataFrame(admin) 
-print('ma df : '+admin_df)
-
 
 data = {
     'nom_utilisateur': ['alice', 'bob', 'charlie'],
@@ -27,9 +26,10 @@ def connexion() :
     df_username = 'admin'
     username =  input("Nom d'utilisateur : " )
     password =  input("mot de passe: " )
+    # password =  getpass.getpass("mot de passe: " )
+
     if username == df_username and admin_df['username'][0] :
         a = admin_df['password'][0]
-        print(a)
         verif = check_password_hash(a,password)
         print(verif)
         if verif : 
